d242e902963ee5b8d9eee7fbd614834a2fe640f6
SinitsaBogdan <124495463+SinitsaBogdan@users.noreply.github.com>
2024-01-29 22:50:24 +0300

    Initial project

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./assets/scss/index.scss";
+
+import { Header, Footer } from "./components";
+
+export default function App() {
+  return <div className="App">
+    <Header />
+    <Footer />
+  </div>;
+}
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Footer.scss";
+import "./FooterMedia.scss";
+
+export default function Footer(props) {
+  return <footer className="Footer">Hello Footer</footer>;
+}
