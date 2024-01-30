d5ac58963cbbab2ae583a4517bc7faf2a68255fb
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2024-01-30 16:50:16 +0300

    Update App.js
    Размещение компонентов Header и Footer в главном приложении

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./assets/scss/index.scss";
 
+import { Header, Footer } from "./components";
+
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Footer />
+      <Header />
+    </div>
+  );
 }
