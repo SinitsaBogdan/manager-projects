eda8f3b6ab384b610d891a41250ec4c04285f117
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-01-30 22:35:54 +0300

    update: App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "./assets/scss/index.scss";
 
-import { Card, Header, Footer } from "./components";
+import cardData from './data.json';
+
+import { Card, Footer, Header } from "./components";
 
 export default function App() {
     return (
         <div className="App">
             <Header />
+            
+            {
+                cardData.map((card) => <Card content={card}/>)
+            }
             <Footer />
-            <Card />
         </div>
     );
 }
