33f629f4083468dee161258f17627dabc7c14acd
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-01-30 22:37:17 +0300

    update-component : Card

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -2,6 +2,40 @@ import React from "react";
 import "./Card.scss";
 import "./CardMedia.scss";
 
-export default function Card(props) {
-    return <div className="Card">Hello Card</div>;
+export default function Card({ content }) {
+    return (
+        <div className="Card">
+            <div className="column">
+                <p className="number">{content.id}</p>
+                <h2 className="title">{content.title}</h2>
+                <p className="Card__type">{content.site_type}</p>
+            </div>
+
+            {content.description != "" && (
+                <p className="description">{content.description}</p>
+            )}
+            {content.note != "" && <p className="note">{content.note}</p>}
+
+            <div className="row">
+                <div className="wrapper">
+                    <p className="btn">{content.priority}</p>
+                    <p className="btn">{content.execution_status}</p>
+                </div>
+                <div className="wrapper">
+                    <a href={content.url_git} className="btn">
+                        GIT
+                    </a>
+                    <a href={content.url_task_board} className="btn">
+                        TASK BOARD
+                    </a>
+                    <a href={content.url_miro} className="btn">
+                        MIRO
+                    </a>
+                    <a href={content.url_figma} className="btn">
+                        FIGMA
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 }
