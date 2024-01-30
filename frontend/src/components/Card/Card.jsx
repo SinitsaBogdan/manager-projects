6cd9492979fc0714b2051c96a2f75c0f3085397d
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-01-30 21:00:41 +0300

    component : Card

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -2,6 +2,80 @@ import React from "react";
 import "./Card.scss";
 import "./CardMedia.scss";
 
-export default function Card(props) {
-    return <div className="Card">Hello Card</div>;
+import modulesData from "../../modules.json";
+
+import { Description, Modul, Note } from "../";
+
+export default function Card({ content }) {
+    return (
+        <div className="Card">
+            <div className="Card__header">
+                <p className="number">{content.id}</p>
+                <h2 className="Card__title">{content.title}</h2>
+                <p className="Card__type">{content.site_type}</p>
+                <div className="Card__modules">
+                    {content.project_module.length > 0 &&
+                        content.project_module.map((m) => (
+                            <Modul props={modulesData[m]} />
+                        ))}
+                </div>
+            </div>
+
+            <div className="Card__contents">
+                {content.description !== "" && (
+                    <Description content={content.description} />
+                )}
+                {content.note !== "" && <Note content={content.note} />}
+            </div>
+
+            <div className="Card__buttoms">
+                <div className="wrapper">
+                    <p className="btn">{content.priority}</p>
+                    <p className="btn">{content.execution_status}</p>
+                </div>
+
+                <div className="wrapper">
+                    {content.url_doc !== "" && (
+                        <a className="btn" href={content.url_doc}>
+                            ДОКУМЕНТАЦИЯ
+                        </a>
+                    )}
+                    {content.site_pages !== "" && (
+                        <a className="btn" href={content.site_pages}>
+                            СТРАНИЦЫ
+                        </a>
+                    )}
+                    {content.site_components !== "" && (
+                        <a className="btn" href={content.site_components}>
+                            КОМПОНЕНТЫ
+                        </a>
+                    )}
+                    {content.data_base__table !== "" && (
+                        <a className="btn" href={content.data_base__table}>
+                            ТАБЛИЦЫ
+                        </a>
+                    )}
+                </div>
+                <div className="wrapper">
+                    {content.url_git !== "" && (
+                        <a href={content.url_git} className="btn">
+                            GIT
+                        </a>
+                    )}
+
+                    {content.url_task_board !== "" && (
+                        <a href={content.url_task_board} className="btn">
+                            TASK BOARD
+                        </a>
+                    )}
+
+                    {content.url_figma !== "" && (
+                        <a href={content.url_figma} className="btn">
+                            FIGMA
+                        </a>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 }
