bcfedd1fe76655461b7ce00826f1333d12e8feab
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-01-31 23:48:50 +0300

    Update index.js
    Update index.js

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,3 +1,8 @@
 // components:
-export {default as Header} from "./Header";
-export {default as Footer} from "./Footer";
+export { default as Card } from "./Card";
+export { default as CardPage } from "./CardPage";
+export { default as Description } from "./Description";
+export { default as Footer } from "./Footer";
+export { default as Header } from "./Header";
+export { default as Note } from "./Note";
+export { default as Task } from "./Task";
