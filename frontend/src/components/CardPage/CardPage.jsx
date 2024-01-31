904b0b24e9e263489cac79316d8b569786e4fdde
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-01-31 23:50:39 +0300

    component : CardPage

---
+++ frontend/src/components/CardPage/CardPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CardPage.scss";
+import "./CardPageMedia.scss";
+
+export default function CardPage({ name, url }) {
+    return (
+        <div className="CardPage">
+            <h2 className="CardPage__name">{name}</h2>
+            <p className="CardPage__url">{url}</p>
+        </div>
+    );
+}
