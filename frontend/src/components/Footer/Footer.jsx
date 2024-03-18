77ad6dba6c0e24924ee56467c5ef25a7dfa4bc15
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2024-03-18 14:22:31 +0300

    refactor : project

--- frontend/src/components/Footer/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './Footer.scss';
-import './FooterMedia.scss';
-
-export default function Footer(props) {
-	return <footer className='Footer'>Hello Footer</footer>;
-}
--- frontend/src/components/Header/Header.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './Header.scss';
-import './HeaderMedia.scss';
-
-export default function Header(props) {
-	return <header className='Header'>Hello Header</header>;
-}
